eeaadb297514e6b714b7de946f0de8cf9cd0c686
Kyle <kylezhong68@gmail.com>
2018-10-18 12:00:22 -0400

    test commit

--- capstone/capstone.py
+++ capstone/capstone.py
@@ -4,12 +4,13 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-@app.route("/", methods=["GET", "POST"])
-def hello(N=5):
-    return "Hello world!"
-    #name = [request.form["data" + str(i)] for i in range(N)]
-    #return render_template("hello.html", name="\n".join(name), N=N)
+# @app.route("/", methods=["GET", "POST"])
+# def hello(N=5):
+#     return "Hello world!"
+#     #name = [request.form["data" + str(i)] for i in range(N)]
+#     #return render_template("hello.html", name="\n".join(name), N=N)
 
+@app.route("/", methods=["GET", "POST"])
 @app.route("/admindashboard", methods=["GET", "POST"])
 def admindashboard():
     return render_template("admindashboard.html",title="Admin Dashboard")
